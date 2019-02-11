176d3e70f6031f9d3ec521d927635c9bddfa7b23
indiehjaerta <cruel@krulltigern.nu>
2019-02-12 00:46:58 +0100

    Added normalize.css

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,8 @@ const Meta = () => (
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta charSet="utf-8" />
 		<title>Hype-gear</title>
+		<link rel="stylesheet" type="text/css" href="/static/normalize.css" />
 	</Head>
-)
+);
 
 export default Meta;
