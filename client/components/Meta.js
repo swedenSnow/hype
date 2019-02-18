ebea0599a78ee82f0c6a5db8b9daa23e26b94a0e
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-02-18 12:24:13 +0100

    :art: added selctions of logos and favicons

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -4,6 +4,7 @@ const Meta = () => (
 	<Head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta charSet="utf-8" />
+		<link rel="shortcut icon" href="/static/favicon.png" />
 		<title>Hype-gear</title>
 		<link rel="stylesheet" type="text/css" href="/static/normalize.css" />
 	</Head>
