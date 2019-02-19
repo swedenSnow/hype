02fff39e741f2da71ab085e9dd277d6015e273a7
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-02-19 11:59:08 +0100

    changed some styles for [StyledButton]

--- client/components/styles/Button.js
+++ client/components/styles/Button.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-	background-color: #000;
-	color: #fff;
+	background-color: ${props => props.theme.blue};
+	color: ${props => props.theme.white};
 	border: 0;
 	border-radius: 3px;
-	padding: 0.5rem;
+	padding: 0.8rem 0.5rem;
+
+	/* transform: skew(-10deg);
+	 */
+	text-transform: uppercase;
+	font-size: ${props => props.theme.root};
+	&[disabled] {
+		opacity: 0.5;
+	}
 `;
 
 export { StyledButton };
