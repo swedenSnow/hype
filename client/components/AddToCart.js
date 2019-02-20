9fbe2083cb06fe99a85f679d678ff2f82a98bfea
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-02-20 19:29:06 +0100

    added new com[AddToCart]

--- client/components/AddToCart.js
+++ client/components/AddToCart.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import StyledButton from './styles/StyledButton';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
+const ADD_TO_CART_MUTATION = gql`
+	mutation addToCart($id: ID!) {
+		# //! id of type ID! is going to run this on the server side below
+		addToCart(id: $id) {
+			id
+			quantity
+		}
+	}
+`;
 class AddToCart extends Component {
 	addToCart = () => {
 		alert('One day this will work?');
 	};
 
 	render() {
+		const { id } = this.props;
 		return (
-			<StyledButton onClick={this.addToCart}>Add To Cart</StyledButton>
+			<Mutation
+				mutation={ADD_TO_CART_MUTATION}
+				variables={{
+					id,
+				}}
+			>
+				{addToCart => (
+					<StyledButton onClick={addToCart}>Add To Cart</StyledButton>
+				)}
+			</Mutation>
 		);
 	}
 }
