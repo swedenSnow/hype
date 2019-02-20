2d49a96c9e524d61b48ab6c5a0ca52ccb1c462ab
MikaelFEND17 <Mikael.Larsson2@yh.nackademin.se>
2019-02-20 13:28:53 +0100

    Update Account.js

--- client/components/Account.js
+++ client/components/Account.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import Link from 'next/link';
 import PleaseSignIn from './PleaseSignIn';
 
+const USERINFO_QUERY = gql`
+	query {
+		self {
+			id
+			email
+			firstName
+			lastName
+		}
+	}
+`;
+
 class Account extends Component {
 	render() {
-		console.log(this.props);
 		return (
 			<PleaseSignIn message="Please sign in to view your account.">
-				<div>
-					E-mail: ???
-					<hr />
-					<Link href="/orders">
-						<a>My Orders</a>
-					</Link>
-					Update Details
-					<br />
-					Change Password
-					<br />
-				</div>
+				<Query query={USERINFO_QUERY}>
+					{({ data, loading, error }) => {
+						console.log(data);
+						return (
+							<div>
+								<strong>E-mail:</strong> {data.self.email}
+								<br />
+								<strong>First name:</strong>
+								{data.self.firstName} <br />
+								<strong>Last name:</strong> {data.self.lastName}
+								<br />
+								<strong>Password:</strong> ilikepenises <br />
+								<hr />
+								<Link href="/orders">
+									<a>My Orders</a>
+								</Link>
+								Update Details
+								<br />
+								Change Password
+								<br />
+							</div>
+						);
+					}}
+				</Query>
 			</PleaseSignIn>
 		);
 	}
